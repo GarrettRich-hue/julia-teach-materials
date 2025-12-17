5c74c34ca8ca73a9b14a0b152be4e677f652b844
Garrett Richards <garrettrichards311@gmail.com>
2025-12-17 11:28:42 +0000

    Getting started

---
+++ notebooks/julia101/01-the-repl.jl
@@ -0,0 +1,34 @@
+### A Pluto.jl notebook ###
+# v0.20.21
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ fcd7027e-db39-11f0-835b-6da0ae99d520
+#
+
+
+# ╔═╡ aa53dc9d-3766-4b9e-a781-b68591290202
+
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.12.1"
+manifest_format = "2.0"
+project_hash = "71853c6197a6a7f222db0f1978c7cb232b87c5ee"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╠═fcd7027e-db39-11f0-835b-6da0ae99d520
+# ╠═aa53dc9d-3766-4b9e-a781-b68591290202
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
